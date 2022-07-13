5b26d99768e5e9988d393768727ca5dc754f27a0
Jonathan Harrison <jonjam1990@googlemail.com>
2022-07-13 08:06:10 +0100

    Switched to angular builds jest

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,9 @@
+describe('HeroesComponent', () => {
+    it('Should do one thing', () => {
+      // do something...
+    });
+  
+    it('Should do another thing', () => {
+      // do something...
+    });
+  });
