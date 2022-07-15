c7d28008472752331019a6cc59ec264c8efc1e5a
Jonathan Harrison <jonjam1990@googlemail.com>
2022-07-15 07:58:44 +0100

    PollyJS working with Angular

--- README.md
+++ README.md
@@ -4,18 +4,31 @@
 Copied from [Tour of Heroes Tutorial](https://angular.io/tutorial/toh-pt6)
 
 # Angular Builders
-https://github.com/just-jeb/angular-builders/tree/master/packages/jest
+Setup jest with Angular using [Angular Builders](https://github.com/just-jeb/angular-builders/tree/master/packages/jest)
 
 ## Angular
-https://angular.io/api/core/testing/ComponentFixture#detectchanges
-https://angular.io/guide/testing-utility-apis#componentfixture-methods
-https://angular.io/guide/testing-components-scenarios#component-with-async-service
+Useful links about Angular testing:
+- https://angular.io/api/core/testing/ComponentFixture#detectchanges
+- https://angular.io/guide/testing-utility-apis#componentfixture-methods
+- https://angular.io/guide/testing-components-scenarios#component-with-async-service
 
 ## Spectator
-https://ngneat.github.io/spectator/docs/installation
-https://ngneat.github.io/spectator/docs/testing-with-routing
-https://ngneat.github.io/spectator/docs/testing-modules
+Setup Spectator using:
+- https://ngneat.github.io/spectator/docs/installation
+- https://ngneat.github.io/spectator/docs/testing-with-routing
+- https://ngneat.github.io/spectator/docs/testing-modules
 
 ## Angular integration tests
 https://dev.to/cjcoops/how-to-write-simple-angular-integration-tests-with-spectator-1i1b
 https://christianlydemann.com/complete-guide-to-angular-testing/
+
+## PollyJS
+Setup PollyJS by following:
+- https://netflix.github.io/pollyjs/#/quick-start
+- https://netflix.github.io/pollyjs/#/test-frameworks/jest-jasmine
+- https://github.com/Netflix/pollyjs/tree/master/examples/typescript-jest-node-fetch
+- https://netflix.github.io/pollyjs/#/adapters/xhr
+- https://netflix.github.io/pollyjs/#/persisters/fs?id=recordingsdir
+
+## Spotify PollyJS presets
+[Spotify presets for PollyJS](https://github.com/spotify/polly-jest-presets)
--- src/app/heroes/heroes.component.spec.ts
+++ src/app/heroes/heroes.component.spec.ts
@@ -1,16 +1,47 @@
-import { createRoutingFactory } from '@ngneat/spectator/jest';
+import {byRole, createRoutingFactory} from '@ngneat/spectator/jest';
 import {HeroesComponent} from './heroes.component';
 import {AppModule} from '../app.module';
 import { setupPolly } from 'setup-polly-jest';
+import path from 'path';
+import { PollyConfig} from '@pollyjs/core';
+import { MODES } from '@pollyjs/utils'
 
 describe('HeroesComponent', () => {
+  // TODO move to common script
+  let recordIfMissing = true;
+  let mode: PollyConfig['mode'] = MODES.REPLAY;
+
+  switch (process.env['POLLY_MODE']) {
+    case 'record':
+      mode = MODES.RECORD;
+      break;
+    case 'replay':
+      mode = MODES.REPLAY;
+      break;
+    case 'offline':
+      mode = MODES.REPLAY;
+      recordIfMissing = false;
+      break;
+  }
+
   let context = setupPolly({
+      recordIfMissing,
+      mode,
       // Having to use require imports due to https://github.com/gribnoysup/setup-polly-jest/issues/23
-      adapters: [require('@pollyjs/adapter-xhr')],// TODO Uninstall require('@pollyjs/adapter-node-http')],
-      persister: require('@pollyjs/persister-rest'),
-      logLevel: 'info' // Log requests to console
+      adapters: [require('@pollyjs/adapter-xhr')],
+      persister: require('@pollyjs/persister-fs'),
+      persisterOptions: {
+       fs: {
+         recordingsDir: path.resolve(__dirname, '__recordings__'),
+       },
+       disableSortingHarEntries: true
+      },
+      flushRequestsOnStop: true,
+      recordFailedRequests: true,
+      // Default level is warn. Useful to see PollyJs Recorded or Replayed logs
+      logLevel: 'info'
+    // TODO Expires setup
   });
-  // TODO Work out why snapshot failing
 
   const createComponent = createRoutingFactory({
     component: HeroesComponent,
@@ -19,10 +50,8 @@ describe('HeroesComponent', () => {
     detectChanges: false,
   });
 
-  // NOTE: This will make real API requests now
-  it('renders something', async () => {
-    context.polly.configure({ recordIfMissing: true });
-
+  it('renders a list of heroes', async () => {
+    // ACT
     const spectator = createComponent();
 
     // Trigger ngOnInit
@@ -31,15 +60,43 @@ describe('HeroesComponent', () => {
     // https://angular.io/guide/testing-utility-apis#componentfixture-methods
     spectator.detectChanges();
 
+    // Wait for HTTP calls to resolve
     // https://angular.io/guide/testing-utility-apis#componentfixture-methods
     // https://angular.io/guide/testing-components-scenarios#component-with-async-service
-    // Wait for HTTP call
-    await spectator.fixture.whenStable();
+    // await spectator.fixture.whenStable();
+    // https://netflix.github.io/pollyjs/#/api?id=flush
+    // Need to use this otherwise promises won't resolve until after polly is stopped when test completes
+    await context.polly.flush();
 
     // Update view with heroes data
     spectator.detectChanges();
 
-    // TODO Change to not be snapshot
-    expect(spectator.fixture).toMatchSnapshot();
+    // ASSERT
+    const heroes : HTMLLIElement[] = spectator.queryAll(byRole('listitem'));
+
+    const linkContents = heroes.map(li => {
+      const link = li.querySelector('a');
+
+      if (link === null || link.textContent === null) {
+        return "";
+      }
+
+      return link.textContent.trim();
+    });
+
+    const expected =  [
+      '1 Leanne Graham',
+      '2 Ervin Howell',
+      '3 Clementine Bauch',
+      '4 Patricia Lebsack',
+      '5 Chelsey Dietrich',
+      '6 Mrs. Dennis Schulist',
+      '7 Kurtis Weissnat',
+      '8 Nicholas Runolfsdottir V',
+      '9 Glenna Reichert',
+      '10 Clementina DuBuque'
+    ];
+
+    expect(linkContents).toEqual(expect.arrayContaining(expected));
   });
 });
