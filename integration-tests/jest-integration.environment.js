bec93ad1d444ae8c88984eab853612b18ed2529f
Jonathan Harrison <jonjam1990@googlemail.com>
2022-10-05 05:59:10 +0100

    Fixing comment

--- integration-tests/jest-integration.environment.js
+++ integration-tests/jest-integration.environment.js
@@ -5,7 +5,7 @@ class JestIntegrationEnvironment extends SetupPollyJestEnvironment {
   constructor(config, context) {
     super(config, context);
 
-    // Setting up testPath global for auto-setup-polly.ts
+    // Setting up testPath global for pollyContext.ts
     // Followed https://stackoverflow.com/questions/62995762/how-do-you-find-the-filename-and-path-of-a-running-test-in-jest
     this.global.testPath = context.testPath;
   }
